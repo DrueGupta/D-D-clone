4ea5b29bd789f46af223231cdde0005064848f2e
Drue Gupta <1031762@my.scccd.edu>
2024-11-21 11:12:39 -0800

    Create Shank.cpp

---
+++ Shank.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include "Weapon.h"
+#include "Shank.h"
+#include "Effects.h"
+using namespace std;
+
+Shank::Shank()
+{
+	this->name = "Bane of London";
+	this->damage = 10;
+	this->durability = 100;
+	this->rarity = "Rare";
+}
+void Shank::equip(Player* owner)
+{
+	owner->dexterity += 10;
+}
+bool Shank::weaponSpecial(Player* owner, Player* target, string type)
+{
+	int dmg = rand() % 8 + 1;
+	int dur = rand() % 5 + 1;
+	Effect p("Posion", dmg, dur, owner);
+	cout << target->name << " has been poisoned by the Bane of London" << endl;
+	target->effects.push_back(effect);
+}
